4b8dba5dfd0a90fe2cbac330d15cfc51cf8bd58f
Rahul2322 <rohitmsingh86@gmail.com>
2023-11-27 11:14:29 +0545

    currency converter

---
+++ currencyConverter/src/components/InputBox.jsx
@@ -0,0 +1,49 @@
+
+
+const InputBox = ({
+     onCurrencyChange, 
+     label, 
+     amount, 
+     currencyOptions = [],
+      selectCurrency, 
+      onAmountChange,
+      amountDisable=false ,
+      currencyDisable = false,
+    }) => {
+console.log(amount,'amount')
+    return (
+        <div className='bg-white p-3 rounded-lg text-sm flex'>
+            <div className='w-1/2'>
+                <label htmlFor="" className="text-black/40 mb-2 inline-block" >{label}</label>
+                <input
+                className="outline-none w-full bg-transparent py-1.5"
+                    type="number"
+                    placeholder="Amount"
+                    onChange={(e)=>onAmountChange && onAmountChange(Number(e.target.value))}
+                    value={amount}
+                    disabled={amountDisable}
+                   
+                />
+            </div>
+            <div className="w-1/2 flex flex-wrap justify-end text-right">
+               <p className="text-black/40 mb-2 w-full">Currency Type</p>
+                <select 
+                value={selectCurrency}
+                onChange={(e)=> onCurrencyChange && onCurrencyChange(e.target.value)}
+                disabled={currencyDisable} 
+                >
+                    {currencyOptions.map((currency) => (
+                        <option
+                            key={currency}
+                            value={currency}
+                        >
+                            {currency}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    )
+}
+
+export default InputBox
